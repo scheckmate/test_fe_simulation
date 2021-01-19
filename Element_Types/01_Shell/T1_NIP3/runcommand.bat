c68038116845f02a3ba27f8d9bd378396140174d
ahmedrag01 <65832570+ahmedrag01@users.noreply.github.com>
2021-01-19 11:29:06 +0100

    Add files via upload

---
+++ Element_Types/01_Shell/T1_NIP3/runcommand.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\LSTC\LS-DYNA\ls-dyna_smp_s_R10.1_winx64_ifort131.exe" i=D:\Masterstudium_Maschinenbau_TU-Graz\3\FE-Hochdynamische\03_Element_Types-20210119\Element_Types\01_Shell\T1_NIP3\01_Shell.dyn ncpu=1 memory=20m > lsrun.out.txt 2>&1
